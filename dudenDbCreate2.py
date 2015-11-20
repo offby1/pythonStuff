8c47cb1b6b0ffc25afc76780c748a3bf7b9dbfc8
laufenLesenLernen <pinkardmichael@gmail.com>
2015-11-19 20:45:38 -0800

    a few words

--- dudenDbCreate2.py
+++ dudenDbCreate2.py
@@ -6,4 +6,4 @@ with sqlite3.connect('duden2.db') as conn:
 	c.execute("CREATE TABLE IF NOT EXISTS Woertern(Woertern TEXT, WordID INT PRIMARY KEY)")
 	c.execute("CREATE TABLE IF NOT EXISTS Bedeutung(Bedeutung TEXT, WordID INT, FOREIGN KEY (WordID) REFERENCES Woertern (WordID))")
 	c.execute("CREATE TABLE IF NOT EXISTS Synonyme(Synonym TEXT, WordID Int, FOREIGN KEY (WordID) REFERENCES Woertern (WordID))")
-	
+	c.execute("CREATE TABLE IF NOT EXISTS Beispiele(Beispiel TEXT, WordID Int, FOREIGN KEY (WordID) REFERENCES Woertern (WordID))")
